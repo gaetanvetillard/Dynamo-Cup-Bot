80670f8bfd3b3665651417c5919ea6c3016c8671
Gaetan VETILLARD <ga.vtl53@gmail.com>
2021-04-30 00:54:37 +0200

    add leaderboard function and correct issue

--- main.py
+++ main.py
@@ -1,17 +1,73 @@
 import discord
-from discord import player
-from discord.utils import get
+from sqlalchemy import create_engine, Table, Column, Integer, Float, MetaData, select, desc 
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import json
+import os
 
-DISCORD_TOKEN = 'ODM2NTU5NTg1MjQyMzgyMzY4.YIfw0w.rX5De7MNOedZeD2ZnAE7miv98Vk'
+
+DISCORD_TOKEN = os.environ.get(['DISCORD_TOKEN'])
 MODE = "duo"
 CATEGORY = 720675067532673038
 MAX = 50
 REGISTER_CHANNEL = 719956101109645442
 WELCOME_CHANNEL = 837048983745462342
+LEADERBOARD_CHANNEL = 837025557081882680
+with open("tops.json") as f:
+    TOP = json.load(f)
+    f.close()
 
 
 intents = discord.Intents().all()
-client = discord.Client(intents=intents)
+client = discord.Client(intents=intents, activity=discord.Activity(type=discord.ActivityType.listening, name="!register"))
+
+
+engine = create_engine(os.environ.get(['DATABASE_URL']))
+db = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+class Leaderboard(db):
+    __tablename__ = 'leaderboard'
+
+    id = Column(Integer, primary_key=True)
+    team = Column(Integer)
+    score = Column(Integer)
+    games = relationship("Game", back_populates="team")
+
+
+class Game(db):
+    __tablename__ = 'game'
+
+    id = Column(Integer, primary_key=True)
+    team_id = Column(Integer, ForeignKey("leaderboard.id"))
+    team = relationship("Leaderboard", back_populates="games")
+    top = Column(Integer)
+    kills = Column(Integer)
+    points = Column(Integer)
+
+
+db.metadata.create_all(engine)
+
+def update_leaderboard(guild):
+    all_teams = session.query(Leaderboard).order_by(desc(Leaderboard.score)).all()
+    _message = ""
+    for team in all_teams:
+        #Find players in this team 
+        players = ""
+        for member in guild.members:
+            for role in member.roles:
+                if role.name.split('-')[-1] == str(team.team):
+                    if players == "":
+                        players += member.name
+                    else:
+                        players += " & " + member.name
+        if players == "":
+            players = f"{MODE.capitalize()} {team.team}"
+        _message += f'__{all_teams.index(team) + 1} - {team.score} points :__ **{players}**\n'
+    return _message
+
 
 
 @client.event
@@ -64,17 +120,19 @@ async def on_message(message):
 
             guild = message.guild
             category = guild.get_channel(CATEGORY)
-            all_channels = [channel for channel in category.channels]
+            all_channels = [channel for channel in category.channels if MODE in channel.name]
             i=1
+            position = None
             if len(all_channels) > 0 and len(all_channels) <= MAX:
                 for channel in all_channels:
-                    if int(channel.name[-1]) != i:
+                    if int(channel.name.split('-')[-1]) != i:
                         position = channel.position - 1
                         n=i
                         break
                     i+=1
                 n=i
-                position = channel.position
+                if position == None:
+                    position = channel.position
             else:
                 n = 1
                 position = 0
@@ -94,7 +152,7 @@ async def on_message(message):
             except:
                 pass
             
-            new_embed = discord.Embed(title="Votre équipe à bien été inscrite.", colour=discord.Colour.green())
+            new_embed = discord.Embed(title="Votre équipe a bien été inscrite.", colour=discord.Colour.green())
             new_embed.set_footer(text=f"Host : {message.author}", icon_url=message.author.avatar_url)
             await message.channel.send(embed=new_embed)
 
@@ -116,11 +174,11 @@ async def on_message(message):
                 if MODE in role.name:
                     team_to_delete = role
                     for channel in category.channels:
-                        if channel.name[-1] == role.name[-1] and MODE in channel.name:
+                        if channel.name == role.name:
                             channel_to_delete = channel
 
             if team_to_delete != None and channel_to_delete != None:
-                new_embed = discord.Embed(title="Votre team a bien été désinscrite.", description=f"Désinscription du {role.name.capitalize()}.", colour=discord.Colour.orange())
+                new_embed = discord.Embed(title="Votre équipe a bien été désinscrite.", description=f"Désinscription du {role.name.capitalize()}.", colour=discord.Colour.orange())
                 new_embed.set_footer(text=f"Host : {message.author}", icon_url=message.author.avatar_url)
                 
                 for member in guild.members:
@@ -171,10 +229,63 @@ async def on_message(message):
                 new_embed.set_footer(text=f"Host : {message.author}", icon_url=message.author.avatar_url)
                 await message.channel.send(embed=new_embed)
 
-    if message.content.startswith('$result') and message.channel.category_id == CATEGORY:
+    if message.channel.category_id == CATEGORY:
         if message.content.startswith('$result add') and message.author.guild_permissions.administrator:
-            team = message.channel.name
-            await message.channel.send(team)
+            if len(message.content.split(' ')) == 4:
+                team_num = message.channel.name.split('-')[-1]
+                top = message.content.split(' ')[2]
+                kills = message.content.split(' ')[3]
+                points = TOP[top] + int(kills)
+                #check team in leaderboard
+                team = session.query(Leaderboard).filter_by(team=team_num).first()
+                if team == None:
+                    team = Leaderboard(
+                        team=team_num,
+                        score=0,
+                    )
+                    session.add(team)
+                game_result = Game(
+                    team=team,
+                    top=int(top),
+                    kills=int(kills),
+                    points=points
+                )
+                session.add(game_result)
+                #Update score on leaderboard
+                team.score += points
+                session.commit()
+                await message.add_reaction('✅')
+
+        if message.content.startswith('$leaderboard') and message.author.id == 331405760544112641 and message.channel.id == LEADERBOARD_CHANNEL:
+            await message.channel.send("Classement :")
+
+        if message.content.startswith('$update_leaderboard'):
+            msg = update_leaderboard(message.guild)
+            await message.channel.purge(limit=15)
+            await message.channel.send(f'**Classement :**\n\n{msg}')
+
+
+    if message.author.guild_permissions.administrator:
+        if message.content.startswith('!status'):
+            new_embed = discord.Embed(title="Status", description=f":green_circle: Bot {discord.Status.online}", colour=discord.Colour.red())
+            new_embed.set_footer(text=f"Host : {message.author}", icon_url=message.author.avatar_url)
+            await message.channel.send(embed=new_embed)
+
+        if message.content.startswith('!clear'):
+            if len(message.content.split(' ')) == 2:
+                amount = message.content.split(' ')[-1]
+                try:
+                    amount = int(amount)
+                except ValueError:
+                    amount = 50
+            else:
+                amount = 50
+            await message.channel.purge(limit=amount)
+            new_embed = discord.Embed(title="Success.", description=f"{amount} messages are deleted.", colour=discord.Colour.green())
+            new_embed.set_footer(text=f"Host : {message.author}", icon_url=message.author.avatar_url)
+            await message.channel.send(embed=new_embed)
+
+
 
 
 @client.event
